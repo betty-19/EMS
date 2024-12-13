9108157a6244183f8bfb66ad69749fea93b13c3f
betty-19 <betelhemsintayehu10@gmail.com>
2024-12-13 21:11:22 +0100

    create table inside viewRegistration.jsx

--- src/Components/ViewRegistration.jsx
+++ src/Components/ViewRegistration.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react'
 
+import '../assets/viewRegistration.css'
 function ViewRegistration(){
 
     const [data, setData] =useState([])
@@ -36,7 +37,61 @@ return(
     ))}
  </tbody>
 </table>
+<main className='table'>
+    <section>
+        <h1>Customer's Orders'</h1>
+    </section>
+    <section className='table_body'>
+        <table>
+            <thead>
+                <tr>
+                    <th> id </th>
+                    <th>Customer</th>
+                    <th>Location</th>
+                    <th>Order Date</th>
+                    <th>Status</th>
+                    <th>Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td><img src="../assets/Expenditure.jpg" alt="" /></td>
+                    <td>Addis Ababa</td>
+                    <td>17 Dec, 2022</td>
+                    <td>
+                        <p className='status delivered '>Delivered</p>
+                    </td>
+                    <td>$128.90</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td><img src="../assets/Expenditure.jpg" alt="" /></td>
+                    <td>Addis Ababa</td>
+                    <td>17 Dec, 2022</td>
+                    <td>
+                        <p className='status delivered '>Delivered</p>
+                    </td>
+                    <td>$128.90</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td><img src="../assets/Expenditure.jpg" alt="" /></td>
+                    <td>Addis Ababa</td>
+                    <td>17 Dec, 2022</td>
+                    <td>
+                        <p className='status Cancelled '>Cancelled</p>
+                    </td>
+                    <td>$128.90</td>
+                </tr>
+            </tbody>
+        </table>
+
+    </section>
+    
+</main>
     </div>
+   
 );
 }
 export default ViewRegistration;
